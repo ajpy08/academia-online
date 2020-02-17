a13deee9dc4a6ba6bddb9d3694d62ac3bea9be87
I.S.C. Angel Javier Puc Yamá <angelpuc08@gmail.com>
2020-02-17 16:19:10 -0600

    It was created in mutation.ts a method for new Course

--- src/resolvers/mutation.ts
+++ src/resolvers/mutation.ts
@@ -1,8 +1,28 @@
 import { IResolvers } from "graphql-tools";
 import _ from "lodash";
+import { constants } from "buffer";
+import { database } from "../data/data.store";
 
-const matation: IResolvers = {
-  
+const mutation: IResolvers = {
+  Mutation: {
+      cursoNuevo(__:void, {curso}): any {
+          const itemCurso = {
+              id: String(database.cursos.length + 1),
+              title: curso.title,
+              description: curso.description,
+              clases: curso.clases,
+              time: curso.time,
+              level: curso.level,
+              logo: curso.logo,
+              path: curso.path,
+              teacher: curso.teacher,
+              reviews: []
+          }
+
+          database.cursos.push(itemCurso);
+          return itemCurso;
+      }
+  }
 };
 
-export default matation;
+export default mutation;
