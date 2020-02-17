75e93d806bd5f4a2e09f6c451f52a9092ebe3fe6
I.S.C. Angel Javier Puc Yamá <angelpuc08@gmail.com>
2020-02-17 13:10:17 -0600

    Made changes for add method estudiante(id)

--- src/resolvers/query.ts
+++ src/resolvers/query.ts
@@ -1,12 +1,26 @@
-import { database } from './../data/data.store';
+import { database } from "./../data/data.store";
 import { IResolvers } from "graphql-tools";
 
-const query : IResolvers = {
-    Query: {
-        estudiantes(): any {
-            return database.estudiantes;
-        }
+const query: IResolvers = {
+  Query: {
+    estudiantes(): any {
+      return database.estudiantes;
+    },
+    estudiante(__: void, { id }): any {
+      const resultado = database.estudiantes.filter(
+        estudiante => estudiante.id === id
+      )[0];
+      if (resultado === undefined) {
+        return {
+          id: "-1",
+          name: `No se ha encontrado el estudiante con el Id ${id}`,
+          email: "",
+          courses: []
+        };
+      }
+      return resultado;
     }
-}
+  }
+};
 
 export default query;
