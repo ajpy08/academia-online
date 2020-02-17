fed32050efebcd0bb98ba9211a79336d432acdea
Angel Javier Puc Yamá <angelpuc08@gmail.com>
2020-02-16 22:28:01 -0600

    It was configurated for apollo server express

--- src/server.ts
+++ src/server.ts
@@ -2,15 +2,25 @@ import express from 'express';
 import compression from 'compression';
 import cors from 'cors';
 import { createServer } from 'http';
+import { ApolloServer } from "apollo-server-express";
+import schema from './schema';
+import expressPlayGround from 'graphql-playground-middleware-express';
 
 const app = express();
 
 app.use('*', cors());
 app.use(compression());
 
-app.get('/', (req, res) => {
-    res.send('Hola a la academia online en GraphQL');
+const servidor = new ApolloServer ({
+    schema,
+    introspection: true
 });
+
+servidor.applyMiddleware({app});
+
+app.get('/', expressPlayGround({
+    endpoint: '/graphql'
+}));
 
 const httpServer = createServer(app);
 
@@ -18,6 +28,6 @@ const PORT = 5200;
 httpServer.listen({
     port: PORT
 },
-() => console.log(`Servidor academia online listo http://localhost:${PORT}
+() => console.log(`Servidor academia online listo http://localhost:${PORT}/graphql
 }`)
 );
