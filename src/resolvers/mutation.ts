216ac179e9ed8e02f8d8fb378a1d4145d31bab06
I.S.C. Angel Javier Puc Yamá <angelpuc08@gmail.com>
2020-02-17 17:43:41 -0600

    It was validated if the title of course is the same or already exist, the course doesnt be added.

--- src/resolvers/mutation.ts
+++ src/resolvers/mutation.ts
@@ -5,23 +5,40 @@ import { database } from "../data/data.store";
 
 const mutation: IResolvers = {
   Mutation: {
-      cursoNuevo(__:void, {curso}): any {
-          const itemCurso = {
-              id: String(database.cursos.length + 1),
-              title: curso.title,
-              description: curso.description,
-              clases: curso.clases,
-              time: curso.time,
-              level: curso.level,
-              logo: curso.logo,
-              path: curso.path,
-              teacher: curso.teacher,
-              reviews: []
-          }
+    cursoNuevo(__: void, { curso }): any {
+      const itemCurso = {
+        id: String(database.cursos.length + 1),
+        title: curso.title,
+        description: curso.description,
+        clases: curso.clases,
+        time: curso.time,
+        level: curso.level,
+        logo: curso.logo,
+        path: curso.path,
+        teacher: curso.teacher,
+        reviews: []
+      };
 
-          database.cursos.push(itemCurso);
-          return itemCurso;
+      if (
+        database.cursos.filter(i => i.title === itemCurso.title).length === 0
+      ) {
+        database.cursos.push(itemCurso);
+        return itemCurso;
       }
+
+      return {
+        id: '-1',
+        title: 'El curso ya existe con este titulo',
+        description: '',
+        clases: -1,
+        time: 0.0,
+        level: 'TODOS',
+        logo: '',
+        path: '',
+        teacher: '',
+        reviews: []
+      }
+    }
   }
 };
 
